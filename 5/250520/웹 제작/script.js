ad7be11b32588ddeb5433d66fb549303abd71d68
윤재준 <fruitman200912@gmail.com>
2025-05-27 22:18:40 +0900

    최종

--- 5/250520/웹 제작/script.js
+++ 5/250520/웹 제작/script.js
@@ -9,31 +9,38 @@ let start = false
 let tId
 let oId = []
 
+document.getElementById("t").onclick = function () {
+  sc += 10
+  document.getElementById("sc_bar").innerText = `${sc}`
+  clearInterval(tId)
+  moveT()
+}
+
 function moveT() {
   ch("t")
   tId = setInterval(() => {
     ch("t")
-    document.getElementById("t").onclick = function () {
-      clearInterval(tId)
-      moveT()
-    }
   }, 500);
 }
+
+document.querySelectorAll(".o").forEach(el => {
+  el.addEventListener("click", function () {
+    sc++
+    document.getElementById("sc_bar").innerText = `${sc}`
+    clearInterval(oId[Number(this.id)])
+    moveO(this.id)
+  })
+})
 
 function moveO(oid) {
   ch(oid)
   oId[Number(oid)] = setInterval(() => {
     ch(oid)
-    document.querySelectorAll(".o").forEach(el => {
-      el.addEventListener("click", function () {
-        clearInterval(oId[Number(this.id)])
-        moveO(this.id)
-      })
-    })
-  }, 2000);
+  }, 1000);
 }
 
 document.getElementById("sc_bar").onclick = function () {
+  document.getElementById("sc_bar").innerText = `${sc}`
   moveT()
   moveO('0')
   moveO('1')
